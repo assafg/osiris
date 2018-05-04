cef7bcc22d181800913fe98e152c99bd4bed308f
Assaf Gannon <assaf.gannon@gmai.com>
2018-05-04 14:39:27 +0300

    updated example

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {
     EventSource: require('./lib/EventSource'),
     DynamoDB: require('./lib/database/dynamodb'),
     MongoDB: require('./lib/database/mongodb'),
+    PostgresDB: require('./lib/database/postgresdb'),
 };
